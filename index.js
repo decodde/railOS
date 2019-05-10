00ebbd1ab6c641028c0b7893b10f375b22ca523d
Fasina Oreoluwa <dannyoma75@gmail.com>
2019-05-10 09:41:03 +0000

    added new node modules and started on the index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express=require('express')
 const app=express();
-const body-parser=require('body-parser')
+var bodyparser=require('body-parser')
+
 app.use(express.static(__dirname+'/views'))
+
+app.set('view engine','pug');
+
+app.get('/',function(req,res){
+    res.render('home')
+})
+app.post('/login',function(req,res){
+    
+})
+app.listen(8090,function(){
+    console.log('railOS server running')
+})
