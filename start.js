504e8d5a2dd8338666e0416a2d6229343d5b94d4
Fasina Oreoluwa <dannyoma75@gmail.com>
2019-08-09 11:13:24 +0000

    update to disable cors

--- start.js
+++ start.js
@@ -26,7 +26,12 @@ app.use(session({
 app.use(express.static(__dirname+"/public"))
 app.set('views',__dirname+"/views")
 app.set('view engine',  'pug');
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "herokuapp.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  next();
+});
 app.get("/",function(req,res){
     var dt={
         role:req.session.role,
